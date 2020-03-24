b51e8c09c8537d1a3bc4550fc9eee01bb72ff100
Ng Hui Ling Iris <irisnghl@gmail.com>
2020-03-24 23:17:56 +0800

    changed serverjs file

--- PersonalFinancePlanningApp/server.js
+++ PersonalFinancePlanningApp/server.js
@@ -1,8 +1,8 @@
-const http=require('http');
-const app = require('./app');
-
-const port = process.env.PORT || 3000
-
-const server = http.createServer(app);
-
-server.listen(port);
+var express = require('express');
+var path = require('path')
+var app = express();
+app.use(express.static(path.resolve(__dirname, "www")));
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+ console.log("listening to Port", app.get("port"));
+});
